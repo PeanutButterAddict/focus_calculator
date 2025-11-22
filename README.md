ebf733235e783c1fba26ac68f5cd895b96dbf8d9
PeanutButterAddict <kdastur22@gmail.com>
2025-11-22 08:39:33 +0530

    Changed to premake.

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-## Calculate distraction (24 hr format)
+# Build (Requies Premake5)
+```bat
+premake5 gmake
+make -C build/
+```
+
+# Calculate distraction (24 hr format)
 ```bat
 ./focus_calculator [start_time] [end_time]
 ```
-## Calculate focus (12 hr format)
+
+# Calculate focus (12 hr format)
 ```bat
 ./focus_calculator [start_time] [end_time] [list of distraction mins in commas -- 50,25,30,...]
 ```
-[![Example]()](https://drive.google.com/file/d/1ti7X965bztupKhaigDTprPrc9V5OhvR1/view?usp=drive_link)
---
+++ premake5.lua
@@ -0,0 +1,34 @@
+-- To use fil-c compiler
+-- local prefix = path.getabsolute("../filc-0.674-linux-x86_64/build/bin")
+-- local clang = premake.tools.clang
+--
+-- clang.tools = {
+-- 	cc = prefix .. "/clang",
+-- }
+--
+-- function clang.gettoolname(cfg, tool)
+-- 	return clang.tools[tool]
+-- end
+
+workspace("PeanutButterAddict")
+configurations({ "debug", "release" })
+location("build")
+toolset("clang")
+
+project("focus_calculator")
+kind("ConsoleApp")
+language("C")
+location("build")
+targetdir("bin")
+
+files({ "src/**.h", "src/**.c" })
+
+filter("configurations:debug")
+-- Currently using asserts to stop certain actions.
+-- targetname("%{prj.name}_%{cfg.buildcfg}")
+defines({ "DEBUG" })
+symbols("On")
+
+filter("configurations:release")
+defines({ "NDEBUG" })
+optimize("On")
