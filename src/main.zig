cd16ad5ec2f143de77a4252328f00a0ebd99dfde
PeanutButterAddict <kdastur22@gmail.com>
2026-06-04 12:21:43 +0530

    Use commas and concantation to format with zig fmt however I like.

--- src/main.zig
+++ src/main.zig
@@ -63,8 +63,13 @@ pub fn main(init: std.process.Init) !void {
     const start_time = try Time.initFromString(allocator, arg_list.items[0]);
     const end_time = try Time.initFromString(allocator, arg_list.items[1]);
     if (arg_list.items.len <= 2) {
-        const dist_time = Time.initFromTotalMin(end_time.toTotalMinutes() - start_time.toTotalMinutes());
-        std.debug.print("Distraction: {d} mins , {d} hrs {d} mins\n", .{ dist_time.toTotalMinutes(), dist_time.hour, dist_time.minute });
+        const dist_time = Time.initFromTotalMin(
+            end_time.toTotalMinutes() - start_time.toTotalMinutes(),
+        );
+        std.debug.print(
+            "Distraction: {d} mins , {d} hrs {d} mins\n",
+            .{ dist_time.toTotalMinutes(), dist_time.hour, dist_time.minute },
+        );
         return;
     }
     // Calculate Distraction Time fom 3rd Arg
@@ -74,11 +79,30 @@ pub fn main(init: std.process.Init) !void {
         dist_total_min += try std.fmt.parseUnsigned(u32, str, 10);
     }
     const dist_time = Time.initFromTotalMin(dist_total_min);
-    const total_time = Time.initFromTotalMin(end_time.toTotalMinutes() - start_time.toTotalMinutes());
+    const total_time = Time.initFromTotalMin(
+        end_time.toTotalMinutes() - start_time.toTotalMinutes(),
+    );
     if (dist_time.toTotalMinutes() > total_time.toTotalMinutes()) {
-        std.debug.print("Total minutes in distraction time cannot be more than total time. \n", .{});
+        std.debug.print(
+            "Total minutes in distraction time cannot be more than total time. \n",
+            .{},
+        );
         return;
     }
-    const focus_time = Time.initFromTotalMin(total_time.toTotalMinutes() - dist_time.toTotalMinutes());
-    std.debug.print("Total: {d} mins, {d} hrs {d} mins, Distraction: {d} mins, {d} hrs {d} mins, Focus:  {d} mins, {d} hrs {d} mins \n", .{ total_time.toTotalMinutes(), total_time.hour, total_time.minute, dist_time.toTotalMinutes(), dist_time.hour, dist_time.minute, focus_time.toTotalMinutes(), focus_time.hour, focus_time.minute });
+    const focus_time = Time.initFromTotalMin(
+        total_time.toTotalMinutes() - dist_time.toTotalMinutes(),
+    );
+    std.debug.print("Total: {d} mins, {d} hrs {d} mins," ++
+        " Distraction: {d} mins, {d} hrs {d} mins," ++
+        " Focus:  {d} mins, {d} hrs {d} mins \n", .{
+        total_time.toTotalMinutes(),
+        total_time.hour,
+        total_time.minute,
+        dist_time.toTotalMinutes(),
+        dist_time.hour,
+        dist_time.minute,
+        focus_time.toTotalMinutes(),
+        focus_time.hour,
+        focus_time.minute,
+    });
 }
